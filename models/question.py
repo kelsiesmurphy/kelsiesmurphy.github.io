99e291f018a6706672d42ad0b0f89574817c437d
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2022-11-23 23:19:09 +0000

    adding quiz project

---
+++ models/question.py
@@ -0,0 +1,8 @@
+class Question():
+
+    def __init__(self,question, correct_answer, answers, difficulty, is_correct):
+        self.question = question
+        self.correct_answer = correct_answer
+        self.answers = answers
+        self.difficulty = difficulty
+        self.is_correct = is_correct
---
+++ models/question_list.py
@@ -0,0 +1,9 @@
+from models.question import *
+
+question1 = Question("Which car manufacturer won the 2017 24 Hours of Le Mans?", "Porsche", ["Toyota","Audi", "Porsche","Chevrolet"], "medium", None)
+question2 = Question("In Star Trek: Voyager, which episode did Voyager establish real-time communication with Starfleet Headquarters?", "Pathfinder", ["Message In A Bottle", "Pathfinder","Someone To Watch Over Me","Counterpoint"], "easy", None)
+question3 = Question("In which Call of Duty game are the Apothicons introduced in the Zombies mode?", "Call Of Duty: Black Ops III", ["Call Of Duty: Black Ops III", "Call Of Duty: Black Ops","Call Of Duty: World At War","Call Of Duty: Black Ops II"], "medium", None)
+question4 = Question("How many paint and pastel versions of The Scream is Norwegian painter Edvard Munch believed to have produced?", "4", ["1","3","2","4"], "hard", None)
+question5 = Question("Who is attributed credit for recording the epic poem The Odyssey?", "Homer", ["Aristotle","Odysseus", "Homer","Socrates"], "medium", None)
+
+questions = [question1, question2, question3, question4, question5]
